5b9749ab11e76f2fd7d756391f7372ff100415e0
Lukas Mateffy <github@mateffy.me>
2023-08-29 20:17:12 +0200

    added data uploads

--- src/Livewire/Finder/Actions/UploadFile.php
+++ src/Livewire/Finder/Actions/UploadFile.php
@@ -4,8 +4,13 @@ namespace Cabinet\Filament\Livewire\Finder\Actions;
 
 use Cabinet\Cabinet;
 use Cabinet\Filament\Livewire\Finder\Actions\Concerns\HasFolder;
+use Cabinet\Sources\Contracts\AcceptsData;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Get;
 use Illuminate\Support\HtmlString;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
@@ -33,16 +38,96 @@ class UploadFile extends \Filament\Actions\Action
         ]);
 
         $this->form([
-            FileUpload::make('files')
-                ->label('Files')
-                ->multiple()
-                ->saveUploadedFileUsing(fn (Cabinet $cabinet, TemporaryUploadedFile $file) =>
-                    $this->upload($cabinet, $file)
+            Select::make('form')
+                ->hiddenLabel()
+                ->label('Art')
+                ->default('spatie-media')
+                ->live()
+                ->options(fn (Cabinet $cabinet) => $cabinet->getSourceOptions())
+                ->selectablePlaceholder(false)
+                ->required(),
+            Group::make([])
+                ->schema(fn (Get $get, Cabinet $cabinet) => $get('form') && ($form = $cabinet->getSourceForm($get('form')))
+                    ? match ($form) {
+                        'upload' => [
+                            FileUpload::make('files')
+                                ->label('Files')
+                                ->multiple()
+                                ->saveUploadedFileUsing(fn (Cabinet $cabinet, TemporaryUploadedFile $file) =>
+                                    $this->upload($cabinet, $file)
+                                )
+                                ->required()
+                        ],
+                        default => $form ?? []
+                    }
+                    : []
                 )
-                ->required()
+
+//            match ($get('form')) {
+//                    'spatie-media' => [
+//                        FileUpload::make('files')
+//                            ->label('Files')
+//                            ->multiple()
+//                            ->saveUploadedFileUsing(fn (Cabinet $cabinet, TemporaryUploadedFile $file) =>
+//                                $this->upload($cabinet, $file)
+//                            )
+//                            ->required()
+//                    ],
+//                    'youtube' => [
+//                        TextInput::make('url')
+//                            ->label('YouTube URL')
+//                            ->placeholder('https://www.youtube.com/watch?v=...')
+//                            ->url()
+//                            ->helperText('YouTube URL einfügen')
+//                            ->required()
+//                    ],
+//                    'matterport' => [
+//                        TextInput::make('name')
+//                            ->label('Name')
+//                            ->placeholder('z.B. 2. Stockwerk')
+//                            ->maxLength(255)
+//                            ->required(),
+//                        TextInput::make('url')
+//                            ->label('Matterport URL')
+//                            ->placeholder('https://my.matterport.com/show/?m=...')
+//                            ->url()
+//                            ->helperText('Matterport URL einfügen')
+//                            ->required()
+//                    ],
+//                    'camera-feed' => [
+//                        TextInput::make('name')
+//                            ->label('Name')
+//                            ->placeholder('z.B. Kranansicht Osten')
+//                            ->maxLength(255)
+//                            ->required(),
+//                        TextInput::make('url')
+//                            ->label('Kamera-URL')
+//                            ->placeholder('https://...')
+//                            ->url()
+//                            ->helperText('Kamera-URL einfügen')
+//                            ->required()
+//                    ],
+//                }
         ]);
 
         $this->action(function (Cabinet $cabinet, array $data, UploadFile $action) {
+            $form = $data['form'];
+            $folder = $action->getParentFolder();
+
+            if ($form === 'spatie-media') {
+                $action->success();
+                return;
+            }
+
+            $source = $cabinet->getSource($form);
+
+            if (in_array(AcceptsData::class, class_implements($source))) {
+                /**
+                 * @var AcceptsData $source
+                 */
+                $source->add($folder, $data);
+            }
+
             $action->success();
         });
     }
