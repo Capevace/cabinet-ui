02e960a426e6370d2556714e8b91e1eb5cab5125
Lukas Mateffy <github@mateffy.me>
2023-08-30 11:19:11 +0200

    wip

--- resources/views/components/finder/sidebar-item.blade.php
+++ resources/views/components/finder/sidebar-item.blade.php
@@ -7,12 +7,13 @@
 @endphp
 
 
+
 <button
     {{ $attributes->class([
         'truncate relative font-semibold text-left flex items-center justify-center gap-x-3 rounded-lg px-2 py-2 text-sm text-gray-700 outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:text-gray-200 dark:hover:bg-white/5 dark:focus:bg-white/5',
         'bg-gray-100 text-primary-600 dark:bg-white/5 dark:text-primary-400' => $active,
     ]) }}
-        
+
 
     x-data="{}"
     type="button"
@@ -28,7 +29,7 @@
     />
 
     <span
-        class="flex-1 "
+        class="flex-1 truncate"
         x-show="showSidebar"
     >
         {{ $item->label }}
